8fe51a486235aed593c3e0ff0785d4ee708964ed
pragyachansi <141175618+pragyachansi@users.noreply.github.com>
2025-07-28 10:58:27 -0500

    Create install-avd.ps1

---
+++ install-avd.ps1
@@ -0,0 +1,14 @@
+param(
+  [Parameter(Mandatory)][string] $RegistrationToken
+)
+
+$agentUri = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
+$bootloaderUri = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"
+$agentPath = "$env:TEMP\avdagent.msi"
+$bootloaderPath = "$env:TEMP\avdbootloader.msi"
+
+Invoke-WebRequest -Uri $agentUri -OutFile $agentPath
+Invoke-WebRequest -Uri $bootloaderUri -OutFile $bootloaderPath
+
+Start-Process msiexec.exe -ArgumentList "/i `"$agentPath`" /quiet /qn /norestart REGISTRATIONTOKEN=$RegistrationToken" -Wait
+Start-Process msiexec.exe -ArgumentList "/i `"$bootloaderPath`" /quiet /qn /norestart" -Wait
