fa936e7b679d37b1715650c282cd189572cd14db
pragyachansi <141175618+pragyachansi@users.noreply.github.com>
2025-08-08 09:53:45 -0500

    Update install-avd.ps1

--- install-avd.ps1
+++ install-avd.ps1
@@ -2,14 +2,14 @@ param(
   [Parameter(Mandatory)][string] $RegistrationToken
 )
 
-$flagPath = "C:\Windows\Temp\AVDAgentInstalled.flag"
+# $flagPath = "C:\Windows\Temp\AVDAgentInstalled.flag"
 
-Start-Transcript -Path $logFile -Append
-if (Test-Path $flagPath) {
-    Write-Output "AVD agent already installed. Exiting."
-    Stop-Transcript
-    exit 0
-}
+# Start-Transcript -Path $logFile -Append
+# if (Test-Path $flagPath) {
+#     Write-Output "AVD agent already installed. Exiting."
+#     Stop-Transcript
+#     exit 0
+# }
 
 $agentUri = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
 $bootloaderUri = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"
@@ -22,8 +22,8 @@ Invoke-WebRequest -Uri $bootloaderUri -OutFile $bootloaderPath
 Start-Process msiexec.exe -ArgumentList "/i `"$agentPath`" /quiet /qn /norestart REGISTRATIONTOKEN=$RegistrationToken" -Wait
 Start-Process msiexec.exe -ArgumentList "/i `"$bootloaderPath`" /quiet /qn /norestart" -Wait
 
-Write-Output "Installation complete. Creating flag file."
-New-Item -Path $flagPath -ItemType File -Force
+# Write-Output "Installation complete. Creating flag file."
+# New-Item -Path $flagPath -ItemType File -Force
 
-Stop-Transcript
-exit 0
+# Stop-Transcript
+# exit 0
