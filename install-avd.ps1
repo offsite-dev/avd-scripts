bec53e6fca7aec0a7149f8c44d3cc600df359488
pragyachansi <141175618+pragyachansi@users.noreply.github.com>
2025-08-07 21:28:31 -0500

    Update install-avd.ps1

--- install-avd.ps1
+++ install-avd.ps1
@@ -2,6 +2,15 @@ param(
   [Parameter(Mandatory)][string] $RegistrationToken
 )
 
+$flagPath = "C:\Windows\Temp\AVDAgentInstalled.flag"
+
+Start-Transcript -Path $logFile -Append
+if (Test-Path $flagPath) {
+    Write-Output "AVD agent already installed. Exiting."
+    Stop-Transcript
+    exit 0
+}
+
 $agentUri = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
 $bootloaderUri = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"
 $agentPath = "$env:TEMP\avdagent.msi"
@@ -12,3 +21,9 @@ Invoke-WebRequest -Uri $bootloaderUri -OutFile $bootloaderPath
 
 Start-Process msiexec.exe -ArgumentList "/i `"$agentPath`" /quiet /qn /norestart REGISTRATIONTOKEN=$RegistrationToken" -Wait
 Start-Process msiexec.exe -ArgumentList "/i `"$bootloaderPath`" /quiet /qn /norestart" -Wait
+
+Write-Output "Installation complete. Creating flag file."
+New-Item -Path $flagPath -ItemType File -Force
+
+Stop-Transcript
+exit 0
